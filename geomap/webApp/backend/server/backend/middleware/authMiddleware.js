ee3e02a27ff75ecb9af0927cf1ac79ca6120b656
Merry <merrychris.margarico@ssu.edu.ph>
2024-06-19 10:49:46 +0800

    Update on working automated links

--- geomap/webApp/backend/server/backend/middleware/authMiddleware.js
+++ geomap/webApp/backend/server/backend/middleware/authMiddleware.js
@@ -1,43 +1,35 @@
-
 import jwt from 'jsonwebtoken';
 import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
-import log from '../middleware/logger.js';
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
 
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+  if (req.cookies.jwt) {
     try {
-      token = req.headers.authorization.split(' ')[1];
+      token = req.cookies.jwt;
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      log(`Decoded token: ${JSON.stringify(decoded)}`);
 
       req.user = await User.findById(decoded.userId).select('-password');
 
       if (!req.user) {
-        log('Not authorized, user not found');
         res.status(401);
         throw new Error('Not authorized, user not found');
       }
 
-      log(`User in middleware: ${JSON.stringify(req.user)}`);
+      console.log('User authenticated:', req.user); // Debugging log
+
       next();
     } catch (error) {
-      log(`Error verifying token: ${error.message}`);
+      console.error('Token verification failed:', error); // Debugging log
       res.status(401);
       throw new Error('Not authorized, token failed');
     }
   } else {
-    log('No token found');
+    console.error('No token found in cookies'); // Debugging log
     res.status(401);
     throw new Error('Not authorized, no token');
   }
 });
 
 export { protect };
-
-
-
-
--- geomap/webApp/backend/server/backend/server.js
+++ geomap/webApp/backend/server/backend/server.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 // CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:3000', // Replace with your frontend domain
+  origin: 'http://localhost:3000', // Assuming your frontend is running on port 3000
   credentials: true, // Allow cookies to be sent with requests
 };
 app.use(cors(corsOptions));
